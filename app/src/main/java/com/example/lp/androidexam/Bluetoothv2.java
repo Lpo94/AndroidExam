45c1c99e51b69f3579742ca708e187a7242a3caa
LP <Lars.Peter.Olsen@gmail.com>
2017-05-08 17:51:10 +0200

    Fuuuck thiiis Bluuueetoooothh

--- app/src/main/java/com/example/lp/androidexam/Bluetoothv2.java
+++ app/src/main/java/com/example/lp/androidexam/Bluetoothv2.java
@@ -21,23 +21,6 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class Bluetoothv2 extends AppCompatActivity implements AdapterView.OnItemClickListener{
